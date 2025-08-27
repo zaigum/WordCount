115625b2033f0e1a07bd5122a75f4b60df936986
Zaigum Naseem <143170220+zaigum@users.noreply.github.com>
2025-08-27 23:59:23 +0500

    update

--- background.js
+++ background.js
@@ -10,13 +10,13 @@ chrome.browserAction.onClicked.addListener(function(tab) {
    chrome.action.openPopup();
 });
 
-// Example of sending a message to a content script
+ 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (message.action === 'example') {
-    // Do something with the message
+    
     console.log('Received a message from the content script:', message);
 
-    // Send a response back to the content script if needed
+     
     sendResponse({ data: 'Response from background.js' });
   }
 });
